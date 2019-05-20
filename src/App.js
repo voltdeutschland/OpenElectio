376a89b9f53dbd214ccd1e8e58ca517f7f132640
Alexander Kuttig <alexander.kuttig@googlemail.com>
2019-05-20 23:48:09 +0200

    added flow added first component added state persistance to app js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
+// @flow
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Question from './components/question/Question';
+import SharedConstants from './constants/SharedConstants';
 
-class App extends React.Component {
+type Props = {};
+type State = {};
 
-  render = () => {
-      return (
-          <div className="App">
-              <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo"/>
-                  <p>
-                      Edit <code>src/App.js</code> and save to reload.
-                  </p>
-                  <a
-                      className="App-link"
-                      href="https://reactjs.org"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                  >
-                      Learn React
-                  </a>
-              </header>
-          </div>
-      );
-  }
+class App extends React.Component<Props, State>{
+
+    componentWillMount = async () => {
+        let state = localStorage.getItem(SharedConstants.STORAGE_PATH);
+        if(state){
+            try {
+                await this.persistedSetState(JSON.parse(state));
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    };
+
+    persistedSetState = (newState) => {
+        return new Promise((resolve) => {
+            this.setState(newState, () => {
+                localStorage.setItem(SharedConstants.STORAGE_PATH, JSON.stringify(newState));
+                resolve()
+            });
+        });
+    };
+
+    render = () => {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Question/>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
